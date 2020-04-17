6af933d713fd98d8d44d3b1a83972f35a6378734
Mohit Singh Parihar <mohitsinghparihar@gmail.com>
2020-04-18 01:23:29 +0530

    resume start

---
+++ app.js
@@ -0,0 +1,13 @@
+let content = document.getElementById("content");
+
+const headerTemplate = `<h1>${resume.name}</h1>
+                <h2>${resume.dob}</h2>
+                <h2>${resume.emailId}</h2>`;
+            
+const hobbyTemplate = `<ul>
+                        ${resume.hobbies.reduce((a,c) => a + '<li>' + c + '</li>', '')}
+                    </ul>`
+
+
+
+content.innerHTML = headerTemplate + hobbyTemplate;
---
+++ resume.js
@@ -0,0 +1,7 @@
+const resume = {
+    "name" : "Mohit Singh Parihar",
+    "dob" : "15-11-1996",
+    "emailId" : "mohitsinghparihar@gmail.com",
+    "hobbies" : ["football","piano", "drawing"]
+    
+};
