54d453c7aa42335c74135997432f3792e5289899
Mohit Singh Parihar <mohitsinghparihar@gmail.com>
2020-04-18 23:03:46 +0530

    resume first iteration

--- resume.js
+++ resume.js
@@ -1,83 +1,118 @@
 const resume = {
-    "name" : "Mohit Singh Parihar",
-    "title" : "Mechanical Engineer",
-    "email" : "mohitsinghparihar@gmail.com",
-    "phoneNumber" : "+91 8770973964",
-    "website" : "http:// mohitsinghparihar@gmail.com",
-    "objective" : "To make high quality, exciting, and fun web and mobile applications that are challenging, provide oppurtunites to learn new technologies, and allow me to work with highly motivated and talented developers.",
-    "aboutMe" : " Seeking an entry-level oppurtunity wtih an easteemed organisation where I can utilise my skills and enhance learning in th field if work. Capable of mastering new technologies",
-    "github" : "https://github.com/mohitsparihar/",
-    "linkedin" : "www.linkedin.com/in/mohitsparihar",
-    "social" : [
-            {
-                "link" : "https://github.com/mohitsparihar/",
-                "name" : "GitHub"
-            },
-            {
-                "link" : "http://www.linkedin.com/in/mohitsparihar",
-                "name" : "LinkedIn"
-            },
-            {
-                "link" : "https://facebook.com/mohitsinghp",
-                "name" : "Facebook"
-            },
-            {
-                "link" : "http://www.twitter.com/pariharsmohit",
-                "name" : "Twitter"
-            }    
+    "name": "Mohit Singh Parihar",
+    "title": "Mechanical Engineer",
+    "email": "mohitsinghparihar@gmail.com",
+    "phoneNumber": "+91 8770973964",
+    "website": "https://mohitsparihar.github.io/",
+    "objective": "To make high quality, exciting, and fun web and mobile applications that are challenging, provide oppurtunites to learn new technologies, and allow me to work with highly motivated and talented developers.",
+    "aboutMe": " Seeking an entry-level oppurtunity wtih an easteemed organisation where I can utilise my skills and enhance learning in th field if work. Capable of mastering new technologies",
+    "github": "https://github.com/mohitsparihar/",
+    "linkedin": "www.linkedin.com/in/mohitsparihar",
+    "social": {
+        "github" :{
+            "link": "https://github.com/mohitsparihar/",
+            "name": "GitHub"
+        },
+        "linkedin" : {
+            "link": "http://www.linkedin.com/in/mohitsparihar",
+            "name": "LinkedIn"
+        },
+        "facebook" : {
+            "link": "https://facebook.com/mohitsinghp",
+            "name": "Facebook"
+        },
+        "twitter" : {
+            "link": "http://www.twitter.com/pariharsmohit",
+            "name": "Twitter"
+        }
+    },
+    "skills": [
+        "C++",
+        "HTML5",
+        "CSS3",
+        "Javascript",
+        "MATLAB"
     ],
-    "skills" : [
-                "C++",
-                "Matlab",
-                "HTML5",
-                "CSS3",
-                "Javascript"        
+    "subjects" : [ 
+        "Thermodynamics",
+        "Engineering Mechanics",
+        "IC Engine"
     ],
-    "education" : [
-            {
-                "school" : "Maharana Pratap College of Technolog, Gwalior, India",
-                "startDate" : "2014",
-                "endDate" : "2018",
-                "program" : "Bachelor of Engineering in Mechanical Engineering",
-                "achievements" : "7.4 CGPA(Cumulative Grade Point Average)"
-            },
-            {
-                "school" : "Kendriya Vidyalaya No.2 Vasco Da Gama, Goa",
-                "startDate" : "2013",
-                "endDate" : "2014",
-                "program" : "High School",
-                "achievements" : "8.317 CPI(Cumulative Performance Index)/ 79.02%(Percentage)"
-            },
-            {
-                "school" : "Kendriya Vidyalaya No.2 Vasco Da Gama, Goa",
-                "startDate" : "2011",
-                "endDate" : "2012",
-                "program" : "Secondary School",
-                "achievements" : "10 CGPA(Cumulative Grade Point Average)"
-            },
+    "education": [
+        {
+            "school": "Maharana Pratap College of Technology, Gwalior, India",
+            "startDate": "2014",
+            "endDate": "2018",
+            "program": "Bachelor of Engineering in Mechanical Engineering",
+            "achievements": "7.4 CGPA(Cumulative Grade Point Average)"
+        },
+        {
+            "school": "Kendriya Vidyalaya No.2 Vasco Da Gama, Goa",
+            "startDate": "2013",
+            "endDate": "2014",
+            "program": "High School",
+            "achievements": "8.317 CPI(Cumulative Performance Index)/ 79.02%(Percentage)"
+        },
+        {
+            "school": "Kendriya Vidyalaya No.2 Vasco Da Gama, Goa",
+            "startDate": "2011",
+            "endDate": "2012",
+            "program": "Secondary School",
+            "achievements": "10 CGPA(Cumulative Grade Point Average)"
+        }
     ],
-    "workExperience" : [
-                    {"company" : "HCL",
-                        "topic" : "MATLAB software"},
-                    {"company" : "Rustamji Institute of Technology",
-                        "topic" : "AUDI Cars",
-                        "description" : "Technology introduced in AUDI cars with respect to the cars available in India"},
-                    {"company" : "Maharana Pratap College of Technology",
-                        "topic" : "Casting and Machining on Lathe",
-                        "description" : "Manufacturing of Bolt using the method of sand casting and machining on Lathe"},
-                    {"company" : "Railway Spring Factory, gwalior",
-                       "topic" : "Manufacturing of railway coach spring",
-                       "description" : "Manufacturing of spring used in railway coaches for suspension from rods to finished product using  hot forging and machining"}
+    "trainings": [
+        {
+            "company": "HCL",
+            "topic": "MATLAB software"
+        },
+        {
+            "company": "Rustamji Institute of Technology",
+            "topic": "AUDI Cars",
+            "description": "Technology introduced in AUDI cars with respect to the cars available in India"
+        },
+        {
+            "company": "Maharana Pratap College of Technology",
+            "topic": "Casting and Machining on Lathe",
+            "description": "Manufacturing of Bolt using the method of sand casting and machining on Lathe"
+        },
+        {
+            "company": "Railway Spring Factory, Gwalior",
+            "topic": "Manufacturing of railway coach spring",
+            "description": "Manufacturing of spring used in railway coaches for suspension from rods to finished product using  hot forging and machining"
+        }
     ],
-    "projects" : [{"title" : "Re-generative Braking",
-                   "description" : "Using the frictional power and the torque wasted as heat in braking form generating power"},
-                {"title" : "Experimental Investigation on Solar Chimney for the Room Ventilation",
-                 "description" : "Experimental analysis of air flow in a room using MATLAB software to calculate the the effect of solar chimney ventilation."}
+    "projects": [{
+        "title": "Re-generative Braking",
+        "description": "Using the torque wasted as heat in braking for generating power for further use in the vehicle "
+    },
+    {
+        "title": "Experimental Investigation on Solar Chimney for the Room Ventilation",
+        "description": "Experimental analysis of air flow in a room using MATLAB software to calculate the the effect of solar chimney ventilation."
+    }],
+
+    "personalProjects" : [{
+        "name" : "",
+        "url" : "",
+        "description" : ""
+    },
+    {
+        "name" : "",
+        "url" : "",
+        "description" : ""
+    },
+    {
+        "name" : "",
+        "url" : "",
+        "description" : ""
+    }
     ],
-    "personalProfile" : [{"dob" : "15 Nov 1996",
-                        "mothersName" : "Smt. Parvati Devi",
-                        "fathersName" : "Sh. G S Parihar",
-                        "nationality" : "Indian"},
-                        {"language" : ["Hindi","English"]},
-                        {"hobbies" : ["Football", "Watching Movies", "Playing Piano"]}]
+    "personalProfile": {
+        "dob": "15 Nov 1996",
+        "mothersName": "Smt. Parvati Devi",
+        "fathersName": "Sh. G S Parihar",
+        "nationality": "Indian",
+        "language": ["Hindi", "English"],
+        "hobbies": ["Football", "Watching Movies", "Playing Piano"]
+    }
 };
