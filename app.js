83cb517f922e065865797c49d1c243e5d54fe253
Mohit Singh Parihar <mohitsinghparihar@gmail.com>
2020-04-18 13:45:47 +0530

    cleaning the repo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let content = document.getElementById("content");
 
 const headerTemplate = `<h1>${resume.name}</h1>
                 <h2>${resume.dob}</h2>
-                <h2>${resume.emailId}</h2>`;
+                <h2>${resume.email}</h2>`;
             
 const hobbyTemplate = `<ul>
                         ${resume.hobbies.reduce((a,c) => a + '<li>' + c + '</li>', '')}
